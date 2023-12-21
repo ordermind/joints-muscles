63783acfc5c522cdf4cafc702a7080e9ce341d05
Kristofer Tengstrom <info@kristofer-tengstrom.se>
2023-12-21 14:50:44 +0100

    Fix styling

--- docs/js/pages/home.js
+++ docs/js/pages/home.js
@@ -2,13 +2,9 @@ export default class HomePage {
     render() {
         return {
             main: `
-<div class="d-flex flex-row justify-content-center">
-    <div class="d-flex flex-column justify-content-center">
-        <div class="d-grid gap-3 | nav-buttons-wrapper">
-            <a href="/joints" class="btn btn-outline-primary btn-lg pt-3 pb-3 pe-5 fs-4 text-start" data-navigo>💡<span class="ms-3">Kennisbank</span></a>
-            <a href="/quiz" class="btn btn-outline-primary btn-lg pt-3 pb-3 pe-5 fs-4 text-start" data-navigo>🗒️<span class="ms-3">Quiz</span></a>
-        </div>
-    </div>
+<div class="d-grid gap-3 m-auto | nav-buttons-wrapper">
+    <a href="/joints" class="btn btn-outline-primary btn-lg pt-3 pb-3 pe-5 fs-4 text-start" data-navigo>💡<span class="ms-3">Kennisbank</span></a>
+    <a href="/quiz" class="btn btn-outline-primary btn-lg pt-3 pb-3 pe-5 fs-4 text-start" data-navigo>🗒️<span class="ms-3">Quiz</span></a>
 </div>
         `.trim(),
         };
--- docs/js/pages/quiz-list.js
+++ docs/js/pages/quiz-list.js
@@ -11,14 +11,10 @@ export default class QuizList {
                 this.#mainMenuBlock.render("quizList"),
             ],
             main: `
-<div class="d-flex flex-row justify-content-center">
-    <div class="d-flex flex-column justify-content-center">
-        <h1 class="display-1 fs-1 text-center">Quiz</h1>
-        <div class="d-grid gap-3 | nav-buttons-wrapper">
-            <a href="/quiz/joints" class="btn btn-outline-primary btn-lg pt-3 pb-3 pe-5 fs-4 text-start" data-navigo>🦾<span class="ms-3">Gewrichten</span></a>
-            <a href="/quiz/muscles" class="btn btn-outline-primary btn-lg pt-3 pb-3 pe-5 fs-4 text-start" data-navigo>💪<span class="ms-3">Spieren</span></a>
-        </div>
-    </div>
+<h1 class="display-1 fs-1 text-center">Quiz</h1>
+<div class="d-grid gap-3 m-auto | nav-buttons-wrapper">
+    <a href="/quiz/joints" class="btn btn-outline-primary btn-lg pt-3 pb-3 pe-5 fs-4 text-start" data-navigo>🦾<span class="ms-3">Gewrichten</span></a>
+    <a href="/quiz/muscles" class="btn btn-outline-primary btn-lg pt-3 pb-3 pe-5 fs-4 text-start" data-navigo>💪<span class="ms-3">Spieren</span></a>
 </div>
         `.trim(),
         };
