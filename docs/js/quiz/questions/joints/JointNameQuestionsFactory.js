00ff19405aa8c6d418743feff169b97b1a9f9f66
Kristofer Tengström <info@kristofer-tengstrom.se>
2023-12-22 18:39:38 +0100

    wip - improve scrolling

--- docs/js/quiz/questions/joints/JointNameQuestionsFactory.js
+++ docs/js/quiz/questions/joints/JointNameQuestionsFactory.js
@@ -34,7 +34,7 @@ export default class JointNameQuestionsFactory {
                     correctAnswer,
                     wrongAnswers,
                     question: `
-<h1 id="question-text" class="display-3 fs-3 mb-1">Welk gewricht is dit?</h1>
+<h1 id="question-text" class="display-3 fs-3 mb-1 mt-n4 pt-4">Welk gewricht is dit?</h1>
 <div class="quiz-image-wrapper">
     <img class="quiz-image" src="${correctJoint.image}" />
 </div>
