54c7e87648a873c8a289766f0dc999edce330806
Kristofer Tengström <info@kristofer-tengstrom.se>
2023-12-22 13:59:29 +0100

    Improve text

--- docs/js/quiz/questions/joints/JointFunctionsQuestionsFactory.js
+++ docs/js/quiz/questions/joints/JointFunctionsQuestionsFactory.js
@@ -96,7 +96,7 @@ export default class JointFunctionsQuestionsFactory {
 <div class="quiz-image-wrapper">
     <img class="quiz-image" src="${joint.image}" />
 </div>
-<h2 class="display-4 fs-4 mt-4 mb-4">Welke spieren zorgen voor <strong>${movement.label}</strong>? Sleep die spieren naar het juiste vak.</h2>
+<h2 class="display-4 fs-4 mt-4 mb-4">Welke spieren zorgen voor <strong>${movement.label}</strong> bij dit gewricht? Sleep die spieren naar het juiste vak.</h2>
                         `.trim(),
                         regions: ["Prime movers", "Overige spieren"],
                         answers: shuffle(
