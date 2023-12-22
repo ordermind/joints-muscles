09ed46273c857525b796010de5fbae7a1b38e6fb
Kristofer Tengström <info@kristofer-tengstrom.se>
2023-12-22 18:42:20 +0100

    Improve scrolling

--- docs/js/quiz/questions/DraggableQuestion.js
+++ docs/js/quiz/questions/DraggableQuestion.js
@@ -152,8 +152,6 @@ export default class DraggableQuestion {
         this.#dragula = dragula(draggableContainers)
             .on("drop", this.onDraggableDrop);
 
-        messageBus.emit("question-answered-correctly")
-
         return wrapper;
     }
 
