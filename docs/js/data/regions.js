3d95e81279cd815057d2eef846215f8848cdb56e
Kristofer Tengström <info@kristofer-tengstrom.se>
2023-12-31 13:36:43 +0100

    Rename spine region label

--- docs/js/data/regions.js
+++ docs/js/data/regions.js
@@ -1,7 +1,7 @@
 import Region from "../data-types/Region.js";
 
 export default [
-    new Region({id: "spine", label: "wervelkolom"}),
+    new Region({id: "spine", label: "romp"}),
     new Region({id: "upper_extremity", label: "bovenste extremiteit"}),
     new Region({id: "lower_extremity", label: "onderste extremiteit"}),
 ];
