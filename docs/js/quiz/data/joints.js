8a310ceeb269c52fba222abf36b9f55a80bdd188
Kristofer Tengström <info@kristofer-tengstrom.se>
2023-12-31 23:52:49 +0100

    Skip joint groups in joint quiz

--- docs/js/quiz/data/joints.js
+++ docs/js/quiz/data/joints.js
@@ -1,4 +1,17 @@
 import { arrJoints } from "../../data/joints.js";
 
+const skipJoints = [
+    "columna_vertebralis",
+    "artt_metacarpophalangeae_2_5",
+    "artt_interphalangeae_proximales_manuum_2_5",
+    "artt_interphalangeae_distales_manuum_2_5",
+    "artt_metatarsophalangeae_2_5",
+    "artt_interphalangeae_proximales_pedum_2_5",
+    "artt_interphalangeae_distales_pedum_2_5",
+];
+
 export default arrJoints
-    .filter(joint => joint.regionId === "torso" && joint.id !== "columna_vertebralis");
+    .filter(joint => {
+        return joint.regionId === "torso"
+        && !skipJoints.includes(joint.id)
+    });
