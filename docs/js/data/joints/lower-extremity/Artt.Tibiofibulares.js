7baf5ae6cac52284ef9bee86a8e33deb43ae962b
Kristofer Tengström <info@kristofer-tengstrom.se>
2024-01-04 20:48:00 +0100

    Rename tibiofibular joints

--- docs/js/data/joints/lower-extremity/Artt.Tibiofibulares.js
+++ docs/js/data/joints/lower-extremity/Artt.Tibiofibulares.js
@@ -3,17 +3,17 @@ import Joint from "../../../data-types/Joint.js";
 export default [
     new Joint(
         {
-            id: "art_tibiofibularis_superior",
+            id: "art_tibiofibularis_proximalis",
             regionId: "lower_extremity",
-            label: "art. tibiofibularis superior",
+            label: "art. tibiofibularis proximalis",
             typeIds: ["junctura_synovialis", "art_simplex", "art_plana"],
             cpp: "maximale dorsaalflexie enkel",
             mlpp: "Knie halverwege flexie-extensie met 10° plantairflexie enkel",
-            image: "./images/joints/art_tibiofibularis_superior.jpeg",
+            image: "./images/joints/art_tibiofibularis_proximalis.jpeg",
             description: `
 Bewegingsmogelijkheden zijn beperkt tot een geringe translatie van de fibula naar proximaal bij dorsaalflexie van de voet.
 
-Dit gewricht vormt samen met de membrana interossea en de syndesmosis tibiofibularis een drietal verbindingen tussen fibula en tibia dat functioneel één geheel vormt.
+Dit gewricht vormt samen met de [Link type="Joint" targetId="membrana_interossea_cruris" label="membrana interossea cruris"] en de [Link type="Joint" targetId="art_tibiofibularis_distalis" label="art. tibiofibularis distalis"] een drietal verbindingen tussen fibula en tibia dat functioneel één geheel vormt.
             `.trim(),
         }
     ),
@@ -25,7 +25,7 @@ Dit gewricht vormt samen met de membrana interossea en de syndesmosis tibiofibul
             typeIds: ["junctura_fibrosa"],
             image: "./images/joints/membrana_interossea_cruris.jpeg",
             description: `
-De membrana interossea cruris is een strak bindweefselvlies dat tussen de tibia en fibula is gespannen. Het vormt samen met [Link type="Joint" targetId="art_tibiofibularis_inferior" label="art. tibiofibularis inferior"] de <em>syndesmosis tibiofibularis</em>.
+De membrana interossea cruris is een strak bindweefselvlies dat tussen de tibia en fibula is gespannen. Het vormt samen met [Link type="Joint" targetId="art_tibiofibularis_distalis" label="art. tibiofibularis distalis"] de <em>syndesmosis tibiofibularis</em>.
 
 Bewegingsmogelijkheden zijn beperkt tot een geringe translatie tussen tibia en fibula.
             `.trim(),
@@ -33,15 +33,15 @@ Bewegingsmogelijkheden zijn beperkt tot een geringe translatie tussen tibia en f
     ),
     new Joint(
         {
-            id: "art_tibiofibularis_inferior",
+            id: "art_tibiofibularis_distalis",
             regionId: "lower_extremity",
-            label: "art. tibiofibularis inferior",
+            label: "art. tibiofibularis distalis",
             typeIds: ["junctura_fibrosa"],
             cpp: "maximale dorsaalflexie enkel",
             mlpp: "10° plantairflexie enkel",
-            image: "./images/joints/art_tibiofibularis_inferior.jpeg",
+            image: "./images/joints/art_tibiofibularis_distalis.jpeg",
             description: `
-Art. tibiofibularis inferior vormt samen met de [Link type="Joint" targetId="membrana_interossea_cruris" label="membrana interossea cruris"] de <em>syndesmosis tibiofibularis</em>.
+Art. tibiofibularis distalis vormt samen met de [Link type="Joint" targetId="membrana_interossea_cruris" label="membrana interossea cruris"] de <em>syndesmosis tibiofibularis</em>.
 
 Bewegingsmogelijkheden zijn beperkt tot een geringe translatie tussen tibia en fibula.
 
