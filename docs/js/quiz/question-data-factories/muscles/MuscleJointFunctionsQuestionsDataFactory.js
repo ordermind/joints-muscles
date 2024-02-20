d72d63ed20e1cf0069a7a9bd3cbd2464c03d95ea
Kristofer Tengström <info@kristofer-tengstrom.se>
2024-02-20 10:14:44 +0100

    Do not display type of rotation both in the item label and in the notes in the quiz

--- docs/js/quiz/question-data-factories/muscles/MuscleJointFunctionsQuestionsDataFactory.js
+++ docs/js/quiz/question-data-factories/muscles/MuscleJointFunctionsQuestionsDataFactory.js
@@ -43,7 +43,7 @@ export default class MuscleJointFunctionsQuestionsDataFactory {
                 }
 
                 const shortAnswerLabel = this.#createShortAnswerLabel(jointFunction);
-                const labelWithNotes = shortAnswerLabel + renderNotesTooltip(jointFunction.notes);
+                const labelWithNotes = shortAnswerLabel + renderNotesTooltip(jointFunction.notes.filter(note => !shortAnswerLabel.includes(note)));
 
                 if(!answers.hasOwnProperty(shortAnswerLabel)) {
                     answers[shortAnswerLabel] = labelWithNotes;
@@ -67,7 +67,7 @@ export default class MuscleJointFunctionsQuestionsDataFactory {
 
         for(const jointFunction of correctMuscle.functions) {
             const shortAnswerLabel = this.#createShortAnswerLabel(jointFunction);
-            const labelWithNotes = shortAnswerLabel + renderNotesTooltip(jointFunction.notes);
+            const labelWithNotes = shortAnswerLabel + renderNotesTooltip(jointFunction.notes.filter(note => !shortAnswerLabel.includes(note)));
 
             if(jointFunction.isPrimeMover) {
                 correctSolution.primeMover[shortAnswerLabel] = labelWithNotes;
