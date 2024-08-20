6e10a589e7dbe0d730f103ae283454e017e6e506
Kristofer Tengstrom <info@kristofer-tengstrom.se>
2024-08-20 20:46:22 +0200

    Fix issue with opening certain links in a new tab

--- docs/js/pages/home.js
+++ docs/js/pages/home.js
@@ -3,8 +3,8 @@ export default class HomePage {
         return {
             main: `
 <div class="d-grid gap-3 m-auto | nav-buttons-wrapper">
-    <a href="/joints" class="btn btn-outline-primary btn-lg pt-3 pb-3 pe-5 fs-4 text-start" data-navigo>💡<span class="ms-3">Kennisbank</span></a>
-    <a href="/quiz" class="btn btn-outline-primary btn-lg pt-3 pb-3 pe-5 fs-4 text-start" data-navigo>🗒️<span class="ms-3">Quiz</span></a>
+    <a href="/joints" class="btn btn-outline-primary btn-lg pt-3 pb-3 pe-5 fs-4 text-start" data-navigo>💡<span class="ms-3 pe-none">Kennisbank</span></a>
+    <a href="/quiz" class="btn btn-outline-primary btn-lg pt-3 pb-3 pe-5 fs-4 text-start" data-navigo>🗒️<span class="ms-3 pe-none">Quiz</span></a>
 </div>
         `.trim(),
         };
