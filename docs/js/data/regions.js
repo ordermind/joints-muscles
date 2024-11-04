364d556be5f903551b39bd265dcbc5ec27da451d
Kristofer Tengström <ordermind@gmail.com>
2024-11-04 14:09:22 +0100

    Add new regions

--- docs/js/data/regions.js
+++ docs/js/data/regions.js
@@ -1,7 +1,9 @@
 import Region from "../data-types/Region.js";
 
 export default [
-    new Region({id: "torso", label: "romp"}),
-    new Region({id: "upper_extremity", label: "bovenste extremiteit"}),
-    new Region({id: "lower_extremity", label: "onderste extremiteit"}),
+    new Region({ id: "torso", label: "romp" }),
+    new Region({ id: "upper_extremity", label: "bovenste extremiteit" }),
+    new Region({ id: "hand", label: "alleen de hand" }),
+    new Region({ id: "lower_extremity", label: "onderste extremiteit" }),
+    new Region({ id: "foot", label: "alleen de voet" }),
 ];
